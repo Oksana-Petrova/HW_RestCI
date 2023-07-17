dc5ac5dc62639a6ade4ca2b5e80c4855b0939c06
Oksana Petrova <petrova-2106@mail.ru>
2023-07-17 23:57:23 +0300

    changed json schema with RUR

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV4 {
     @Test
